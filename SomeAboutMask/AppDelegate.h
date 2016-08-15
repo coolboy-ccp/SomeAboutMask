3cf7de32ea7d140040401176e0630968aa6f4e11
brian-chu <chuchengpeng@ccp.local>
2016-08-15 14:17:43 +0800

    Initial Commit

---
+++ SomeAboutMask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SomeAboutMask
+//
+//  Created by liqunfei on 16/8/15.
+//  Copyright © 2016年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SomeAboutMask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SomeAboutMask
+//
+//  Created by liqunfei on 16/8/15.
+//  Copyright © 2016年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
